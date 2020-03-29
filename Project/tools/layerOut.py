94a39001b48dee20e47781f0a931d1b5b20aad86
Ringo <ry.li@qq.com>
2020-03-29 16:51:22 +0800

    pycharm first

---
+++ Project/tools/layerOut.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import arcpy
+import os
+##########################################################
+#设置目标mxd对象路径
+mxd_path = r'D:\rui\code_analysis\file\GIS\class2101\MD.mxd'
+#设置导出目录
+output_path = r'D:\rui\code_analysis\file\GIS\class2101\jpg'
+#常开图层列表
+lyronlist = ['XZQ_town','XZQ_vill_dt']
+#设置分辨率
+resolution = 300
+##########################################################
+#实例化一个mxd对象
+mxd = arcpy.mapping.MapDocument(mxd_path)
+#将所有图层列出
+lyrlist = arcpy.mapping.ListLayers(mxd)
+#将所有图层名列出
+lyrnlist = [i.name for i in arcpy.mapping.ListLayers(mxd)]
+
+#for循环每个需要打印的图层
+for plyr in [ i for i in lyrnlist if i not in lyronlist]:
+    #设置一个变量，容纳打开的图层对象
+    layon = ''
+    #for循环所有图层，打开需要打开的图层
+    for lyr in lyrlist:
+        if lyr.name in lyronlist:
+            lyr.visible =  True
+        elif lyr.name == plyr:
+            lyr.visible = True
+            layon = lyr
+        else:
+            lyr.visible = False
+    #如果需要打印的图层已经打开，导出图片
+    if layon.visible:
+        arcpy.mapping.ExportToJPEG(mxd,
+                              os.path.join(output_path,'{}.jpg'.format(plyr)),
+                              resolution = resolution)
+        print('{} done!'.format(plyr))
+
+##########################################################
+
+#记得删除mxd实例
+del mxd
+print('done')
