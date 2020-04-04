2a6a3c3dc697c168e737349774ae949abac9816c
Ringo <ry.li@qq.com>
2020-04-04 23:04:23 +0800

    upf third

--- Project/art_crawl/article/article/spiders/upf.py
+++ Project/art_crawl/article/article/spiders/upf.py
@@ -7,24 +7,62 @@ from article.items import ArticleItem
 class WanFang(scrapy.Spider):
     name = "upf"
     start_urls = []
-
     for year in range(2019,2020):
         for month in range(12,13):
-            for date in range(1,30):
+            for date in range(14,18):
                 url = 'http://kns.cnki.net/KCMS/detail/detail.aspx?dbcode=CJFQ\
 &dbname=CJFDLAST2016&filename=CSGH{}{}{}'.format(year,str(month).zfill(2),str(date).zfill(3))
-                # print(url)
                 start_urls.append(url)
 
+
+    # def parse(self, response):
+    #     item = ArticleItem()
+    #     item['title'] = response.xpath('//*[@class="wxTitle"]/h2/text()').extract()[0].strip()
+    #     item['abstract'] = response.xpath('//*[@class="wxBaseinfo"]//*[@id="ChDivSummary"]/text()').extract()[0]
+    #     authors_info =  response.xpath('//div[@class="author"]/span/a/@onclick').extract()
+    #     authors_list = [i.strip()[21:-3].replace('\'','').split(',') for i in authors_info]
+    #     item['author'] = []
+    #     author_url_list = []
+    #     for author in authors_list:
+    #         author_url = 'http://kns.cnki.net/kcms/detail/knetsearch.aspx?sfield=au&skey={}&code={}'.format(author[0],author[1])
+    #         item['author'].append(author[0])
+    #         author_url_list.append(author_url)
+    #         # item['ins'] = author_url
+    #         yield scrapy.Request(author_url,callback=self.parse_author,dont_filter=True) 
+        
+    #     # item['author_num'] =
+    #     # item['ins'] =
+    #     item['date'] = response.xpath('//div[@class="sourinfo"]/p[3]/a/text()').extract()[0].strip().replace(';\r\n','')
+    #     if response.xpath('//div[@class="wxBaseinfo"]/p[2]/label[@id="catalog_FUND"]'):
+    #         keywords = response.xpath('//div[@class="wxBaseinfo"]/p[3]/a/text()').extract()
+    #         item['keywords'] = [i.strip().replace(';','')  for i in keywords]
+    #         fund = response.xpath('//div[@class="wxBaseinfo"]/p[2]/a/text()').extract()
+    #         item['fund'] = [i.strip().replace('；','') for i in fund]
+    #     else:
+    #         keywords = response.xpath('//div[@class="wxBaseinfo"]/p[3]/a/text()').extract()
+    #         item['keywords'] = [i.strip().replace(';','') for i in keywords]
+    #         item['fund'] = []
+    #     print(response)
+    #     return item
+
+    # def parse_author(self,response):
+    #     item = response.meta['item']
+    #     item['ins'] = response.xpath('//div[@class="info"]/p[@class="orgn"]/a/text()').extract()[0]
+    #     return item
+
+
     def parse(self, response):
+        print('=========================================')
         item = ArticleItem()
         item['title'] = response.xpath('//*[@class="wxTitle"]/h2/text()').extract()[0].strip()
-        item['abstract'] = response.xpath('//*[@class="wxBaseinfo"]//*[@id="ChDivSummary"]/text()').extract()[0]
-        item['author'] = response.xpath('//*[@class="wxTitle"]/div[1]/span/a/text()').extract()
-        # item['author_num'] =
-        # item['ins'] =
-        item['date'] = response.xpath('//div[@class="sourinfo"]/p[3]/a/text()').extract()[0].strip().replace(';\r\n','')
-        item['keywords'] = response.xpath('//div[@class="wxBaseinfo"]/p[3]/a/text()').extract()
-        item['fund'] = response.xpath('//div[@class="wxBaseinfo"]/p[2]/a/text()').extract()
-        # print(response)
+        authors_info =  response.xpath('//div[@class="author"]/span/a/@onclick').extract()
+        authors_list = [i.strip()[21:-3].replace('\'','').split(',') for i in authors_info]
+        for author in authors_list:
+            author_url = 'http://kns.cnki.net/kcms/detail/knetsearch.aspx?sfield=au&skey={}&code={}'.format(author[0],author[1])
+            # item['ins'] = author_url
+            yield scrapy.Request(author_url, meta={'item': item},callback=self.parse_author) 
+
+    def parse_author(self,response):
+        item = response.meta['item']
+        item['ins'] = response.xpath('//div[@class="info"]/p[@class="orgn"]/a/text()').extract()[0]
         return item
